76d1c2bf0cca25e5e07ac10b8a75d97f79b7599e
Kyle Sekellick <sekellickkyle@yahoo.com>
2018-12-24 23:47:46 -0500

    imports functions into main app

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Scoreboard/>
+        <Player1/>
+        <Player2/>
+        <Score/>
       </div>
     );
   }
